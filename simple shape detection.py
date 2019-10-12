21e550486b25bec120edb247f50d729530976501
nurchulis <nura3609@gmail.com>
2019-10-12 18:11:56 +0700

    ahshais

--- simple shape detection.py
+++ simple shape detection.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("darah4.jpg", cv2.IMREAD_GRAYSCALE)
+img = cv2.imread("darah.jpg", cv2.IMREAD_GRAYSCALE)
 _, threshold = cv2.threshold(img, 240, 255, cv2.THRESH_BINARY)
 contours, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
